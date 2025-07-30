fefd3cfc7469d56dcaf59e07b77090bee4a1ddbc
rudra-health-ai <rudrakp06@gmail.com>
2025-07-30 13:52:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -57,5 +57,8 @@ def predict():
 
     return jsonify(result)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
